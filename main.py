80ed9776d1fe462ec108df1e7bc218d0d642b43b
basaldua24 <reynaldo.basaldua01@utrgv.edu>
2017-02-09 12:02:34 -0600

    Add files via upload
    Here is the code to make the character run.

--- main.py
+++ main.py
@@ -1,200 +1,138 @@
 import pygame
 import sys
-import random
-from utils import vector2
-from sprite1 import *
+import math
 
+class vector2:
+    def __init__( self, x=0, y=0 ):
+        self.x = x
+        self.y = y
+
+    def xy( self ):
+        return (self.x,self.y)
+
+    def add( self, v2 ):
+        result = vector2()
+        result.x = self.x + v2.x
+        result.y = self.y + v2.y
+        return result
+
+    def sub( self, v2 ):
+        result = vector2()
+        result.x = self.x - v2.x
+        result.y = self.y - v2.y
+        return result
+
+    def scale( self, s ):
+        result = vector2()
+        result.x = self.x * s
+        result.y = self.y * s
+        return result
+
+    def mag( self ):
+        return math.sqrt( self.x * self.x + self.y * self.y )
+   
+    def normalized( self ):
+        result = vector2()
+        m = self.mag()
+        if m == 0:
+            return vector2( 0, 0 )
+        result.x = self.x / m
+        result.y = self.y / m
+        return result
+
+pygame.mixer.pre_init( 44100, -16, 2 )
 pygame.init()
-pygame.display.set_caption( "Run Bronco Run!" ) #Placeholder name
-
-# Colors
-black = 0,0,0
-white = 255,255,255
-
-# Timer
-Clock = pygame.time.Clock()
-
-# Frames Per Second
-FPS = 60
-
-# Background Image
-background = pygame.image.load('background_update.png')
-
-background_size = background.get_size()
-background_rect = background.get_rect()
-
-# Set screen to background dimentions
-screen = pygame.display.set_mode(background_size)
-
-# Player Image
-player = pygame.image.load('character.png')
-
-player_size = player.get_size()
-player_rect = player.get_rect()
-
-player_width, player_height = player_size
-
-# Get background width and height dimentions
-background_width, background_height = background_size
-x = 0
-y = 0
-
-# Second set of x and y used for moving background
-x1 = 0
-y1 = -background_height
-
-# Determines how fast the background moves, in frames
-background_speed = 10
-
-# Sprite Position
-sprite_position_x = (background_width / 2) - (player_width / 2)    # Place sprite in the center of x-axis
-sprite_position_y = background_height - (player_height * 1.8)      # Place sprite a couple of pixels above the bottom border of the y-axis
-
-# Sprite Speed and Relative Position
-sprite_pos_enemy = 0.2 
-sprite_pos_player = 1.5
-
-# Part of Old Code
-# sprite_speed_enemy = 0.8
-# sprite_speed_player = 1
-
-# Random positions for Student Obstacles
-student_sprite_position_x = random.randint(192, 832)
-student_sprite_position_y = random.randint(0, 191)
-
-student_sprite_accel1 = 0.01
-student_sprite_accel2 = 0.02
-student_sprite_accel3 = 0.03
-
-enemy = pygame.image.load('enemy.png')
-enemy_sprite_position_y = background_height - (player_height * sprite_pos_enemy) 
-
-font = pygame.font.SysFont(None, 25)
-
-# Message-to-screen Function
-def message_to_screen(msg,color,y):
-    text = font.render(msg, True, color)
-    text_rect = text.get_rect(center=(background_width/2, (background_height/2) + y))
-    # screen.blit(screen_text, [background_width/2, y])
-    screen.blit(text, text_rect)
-
-# Start Screen 
-def game_intro():
-    
-    intro = True
-
-    while intro:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_c:
-                    intro = False
-                if event.key == pygame.K_q:
-                    pygame.quit()
-                    quit()
-                    
-        screen.fill(black)        
-        message_to_screen("Welcome",white,-95.5)
-        message_to_screen("The objective of this game is to escape the law",white,-50)
-        message_to_screen("Use A to move left, D to move right",white,0)
-        message_to_screen("Use Spacebar to jump",white,50)
-        message_to_screen("Press C to continue, Q to quit",white,95.5)
-
-        pygame.display.update()
-        Clock.tick(FPS)
-
-# Player(self, screen, image, x, y, vx, vy, y_len)
-#list = [Pothole(screen, 195, 50, 0, 0),
-#        Enemy( screen, "enemy.png", sprite_position_x, enemy_sprite_position_y, 0, 1),
-#        Player( screen, "character.png", sprite_position_x, sprite_position_y, 0, 1)
-#       ]
- 
-
-# Game Loop
-def game_loop():
-    
-    # Variables
-    gameExit = False
-    gameOver = False
-
-    x = 0
-    y = 0
-
-    x1 = 0
-    y1 = -background_height
-
-    time = pygame.time.get_ticks()
-
-    # Player(self, screen, image, x, y, vx, vy)
-    list = [#Pothole(screen, 195, 50, 0, 0),
-            Pothole(screen, "pothole.png", 192, 50, 0, 0),
-            Enemy( screen, "enemy.png", sprite_position_x, enemy_sprite_position_y, 0, 1),
-            #Students( screen, "student.png", student_sprite_position_x, student_sprite_position_y, 0, 1, student_sprite_accel1),
-            Students( screen, "student.png", student_sprite_position_x, student_sprite_position_y, 0, 1, student_sprite_accel2), # Student sprite with medium acceleration
-            #Students( screen, "student.png", student_sprite_position_x, student_sprite_position_y, 0, 1, student_sprite_accel3),
-            Player( screen, "character.png", sprite_position_x, sprite_position_y, 0, 1)
-           ]
-
-    # while game has not been closed
-    while not gameExit:
-        # when player has been caught (work in progress)
-        while gameOver == True:
-            screen.fill(black)
-            message_to_screen("Game over, press C to play again or Escape key to quit", white,0)
-            pygame.display.update()
-
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        gameExit = True
-                        gameOver = False
-
-                    if event.key  == pygame.K_c:
-                        game_loop()
-                        
-        # Exit Game by pressing the Escape Key            
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                gameExit = True
-                pygame.quit()
-                #sys.exit()
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                gameExit = True
-                # gameOver = False
-                pygame.quit()
-                sys.exit()
+screen = pygame.display.set_mode( (600,225) )
 
 
-        # Background Loop        
-        y1 += background_speed
-        y += background_speed
-        screen.blit(background,(x,y))
-        screen.blit(background,(x1,y1))
-        if y > background_height:
-            y = -background_height
-        if y1 > background_height:
-            y1 = -background_height
+img = pygame.image.load( "run_character.png" ).convert_alpha()
+MARGIN = 200
+IMG_W = 200
+
+frame = 0
+frame_timer = 0
+FRAME_TIME = 90
+FRAME_CT = 4
+
+loc = vector2(120,300)
+v = vector2(0,0)
+SPEED = 0.2
+
+#dp = pygame.mixer.Sound( "shoryuken.wav" )
+
+#pygame.mixer.music.load( "theme.wav" )
+#pygame.mixer.music.play()
+
+start_time = pygame.time.get_ticks()
+
+while True:
+    pygame.event.pump()
+    for evt in pygame.event.get():
+        if evt.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif evt.type == pygame.KEYDOWN and evt.key == pygame.K_ESCAPE:
+            pygame.quit()
+            sys.exit()
+        elif evt.type == pygame.KEYDOWN and evt.key == pygame.K_SPACE:
+            #dp.play()
+            frame = 1
+            frame_timer = 0
+            v.y = 0 #-0.3
+
+    delta = pygame.time.get_ticks() - start_time
+    start_time = pygame.time.get_ticks()
+
+    if loc.y == 80:
+        #grounded
+        inputX = 0
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            inputX -= 1
+        if keys[pygame.K_d]:
+            inputX += 1
+
+        if inputX != 0:
+            v.x = inputX * SPEED
+        elif v.x > 0:
+            v.x = v.x - 0.001 * delta
+            v.x = 0
+        elif v.x < 0:
+            v.x = v.x + 0.001 * delta
+            if v.x > 0:
+                v.x = 0
+            
+    else:
+        # in the air
+        v.y = v.y + 0.001 * delta
+
+    loc = loc.add( v.scale( delta ) )
+
+    if loc.y > 65:
+        loc.y = 65
+        v.y = 0
+
+    clip = pygame.Rect( MARGIN+IMG_W*frame, 0, IMG_W, 160 )
+
+    #if frame > -1:
+    #    if frame_timer > FRAME_TIME:
+    #        frame_timer -= FRAME_TIME
+    #        frame = (frame+1) % FRAME_CT
+    #        print "Frame:" , frame
+    #    frame_timer += delta
+
+    print "frame:" , frame
+    if frame >= 0:
+        if frame_timer > FRAME_TIME:
+            frame_timer -= FRAME_TIME
+            frame = (frame+1) % FRAME_CT
+        frame_timer += delta
         
-        for obj in list:
-            obj.update(time)
-            time = pygame.time.get_ticks() * .25
-           
-        gameOver = obj.checkCollision(list, screen)
-        
-        for obj in list:
-            obj.draw(screen)
-        
-        pygame.display.flip()
-        pygame.display.update()
+    screen.fill( (255, 255, 255) )
 
-        Clock.tick(FPS)
-
-    pygame.quit()   
-    quit()
     
-game_intro()
-game_loop()
+    #screen.blit( stage, (0,0) )
+    screen.blit( img, loc.xy(), area=clip, special_flags=pygame.BLEND_RGBA_MIN )
+
+    pygame.display.flip()
 
